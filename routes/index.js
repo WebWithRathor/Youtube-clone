2974efbbf25280b0a8e0395aa9b53ead4ce7ef53
WebWithRathor <devrajrathor143@gamil.com>
2024-03-04 22:33:35 +0530

    New pages rendered.

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs');
+
+  res.render('index.ejs',{loggedUser: req.user});
 });
-router.get('/profile', function(req, res, next) {
-  res.render('profile.ejs');
+router.get('/channel', function(req, res, next) {
+  res.render('profile.ejs',{loggedUser: req.user});
 });
 router.get('/playlist', function(req, res, next) {
-  res.render('playlist.ejs');
+  res.render('playlist.ejs',{loggedUser: req.user});
 });
-router.get('/channel' ,function(req, res, next) {
-  res.render('channel.ejs');
+router.get('/studio' ,function(req, res, next) {
+  res.render('studio.ejs',{loggedUser: req.user});
+});
+router.get('/history', function(req, res, next) {
+  res.render('history.ejs',{loggedUser: req.user});
+});
+router.get('/results', function(req, res, next) {
+  res.render('results.ejs',{loggedUser: req.user});
+});
+router.get('/shorts', function(req, res, next) {
+  res.render('shorts.ejs',{loggedUser: req.user});
+});
+
+router.get('/you', function(req, res, next) {
+  res.render('you.ejs',{loggedUser: req.user});
 });
 module.exports = router;
